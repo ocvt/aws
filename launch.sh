743b6019af4803fc2deffc21644e34dcbc93bac7
Paul Walko <paulsw.pw@gmail.com>
2020-11-16 03:24:34 +0000

    rm goatcounter

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ up () {
     --env PGID=1000 \
     --env TZ=US/Eastern \
     --env URL=ocvt.club \
-    --env SUBDOMAINS=www,api,stats \
+    --env SUBDOMAINS=www,api \
     --env VALIDATION=http \
     --volume $PWD/nginx.conf:/config/nginx/nginx.conf:ro \
     --volume $PWD/site-confs:/config/nginx/site-confs:ro \
@@ -30,7 +30,7 @@ up () {
     --env-file $PWD/dolabra.env \
     --volume $PWD/data:/go/src/app/data:rw \
     --network ocvt-net \
-    ocvt/dolabra:1.0.6
+    ocvt/dolabra:1.0.7
   
   docker run \
     --name ocvt-site \
@@ -38,9 +38,7 @@ up () {
     --restart unless-stopped \
     --env-file $PWD/ocvt-site.env \
     --network ocvt-net \
-    ocvt/ocvt-site:1.0.7
-
-  nohup ./goatcounter/goatcounter-v1.4.0-linux-amd64 serve -db sqlite://goatcounter/goatcounter.sqlite3 -listen :8081 -tls none >> ./goatcounter/goatcounter.log 2>&1 &
+    ocvt/ocvt-site:1.0.8
 
   docker system prune -a
 }
@@ -48,18 +46,10 @@ up () {
 down () {
   docker stop nginx ocvt-api ocvt-site
   docker rm -f nginx ocvt-api ocvt-site
-  pkill goatcounter
 }
 
 logs () {
   sudo su -c 'multitail --mergeall /var/lib/docker/containers/*/*.log'
 }
-
-### goatcounter
-
-gc_create () {
-  ./goatcounter/goatcounter-v1.4.0-linux-amd64 create -db sqlite://goatcounter/goatcounter.sqlite3 -domain stats.ocvt.club -email ocvt.webmaster@gmail.com
-}
-
 
 $@
